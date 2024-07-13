466094237175df19719f1d5a4d86218b57e210fd
rosendonchev <rosenad@gmail.com>
2024-07-14 01:26:17 +0300

    update

--- scrape.js
+++ scrape.js
@@ -3,6 +3,7 @@ const { exec } = require('child_process');
 const puppeteer = require('puppeteer');
 
 async function scrape() {
+    exec("clear");
     // Clear the sources.m3u file before writing new data
     fs.writeFileSync('sources.m3u', '', 'utf-8');
     fs.appendFileSync('sources.m3u', "#EXTM3U\n", 'utf-8');
