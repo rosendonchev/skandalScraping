291be6850f767f952dab2811ed34ffd6202fe559
rosendonchev <136005518+rosendonchev@users.noreply.github.com>
2024-08-12 07:25:44 +0300

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -2,6 +2,76 @@ const fs = require('fs');
 const { exec } = require('child_process');
 const puppeteer = require('puppeteer');
 
+// Mapping of URL IDs to specific channel names required for the EPG
+const channelNameMapping = {
+    "hd-bnt-1-hd": "BNT1",
+    "bnt-2": "BNT2",
+    "hd-bnt-3-hd": "BNT3",
+    "bnt-4": "BNT4",
+    "hd-nova-tv-hd": "Nova",
+    "hd-btv-hd": "bTV",
+    "hd-btv-action-hd": "bTVAction",
+    "btv-cinema": "bTVCinema",
+    "btv-comedy": "bTVComedy",
+    "btv-story": "bTVStory",
+    "bulgaria-on-air": "BulgariaOnAir",
+    "cartoon-network": "CartoonNetwork",
+    "city-tv": "City",
+    "hd-code-fashion-tv-hd": "CodeFashion",
+    "diema-family": "DiemaFamily",
+    "hd-diema-sport-hd": "DiemaSport",
+    "hd-diema-sport-2-hd": "DiemaSport2",
+    "hd-diema-sport-3-hd": "DiemaSport3",
+    "diema": "Diema",
+    "disney-channel": "Disney",
+    "hd-discovery-channel-hd": "Discovery",
+    "dstv": "DSTV",
+    "e-kids": "EKids",
+    "hd-epic-drama-hd": "EpicDrama",
+    "hd-eurosport-1-hd": "Eurosport1",
+    "hd-eurosport-2-hd": "Eurosport2",
+    "euronews-bulgaria": "EuroNews",
+    "evrokom": "Eurocom",
+    "folklor-tv": "FolklorTV",
+    "hd-food-network-hd": "FoodNetwork",
+    "hd-star-crime-hd": "STARCrime",
+    "hd-star-channel-hd": "STARChannel",
+    "hd-star-life-hd": "STARLife",
+    "hd-id-xtra-hd": "ID",
+    "kanal-3": "Kanal3",
+    "kino-nova": "KinoNova",
+    "hd-max-sport-1-hd": "MAXSport1",
+    "hd-max-sport-2-hd": "MAXSport2",
+    "hd-max-sport-3-hd": "MAXSport3",
+    "hd-max-sport-4-hd": "MAXSport4",
+    "hd-nat-geo-hd": "NatGeo",
+    "hd-nat-geo-wild-hd": "NatGeoWild",
+    "nick-jr": "NickJr",
+    "nickelodeon": "Nickelodeon",
+    "nicktoons": "Nicktoons",
+    "hd-nova-news-hd": "NovaNews",
+    "planeta-folk": "PlanetaFolk",
+    "hd-planeta-hd": "Planeta",
+    "hd-ring-bg-hd": "RING",
+    "rodina-tv": "Rodina",
+    "hd-78-tv-hd": "78TV",
+    "skat": "Skat",
+    "the-voice": "TheVoice",
+    "tiankov-tv": "TiankovFolk",
+    "tlc": "TLC",
+    "hd-travel-channel-hd": "TravelChannel",
+    "travel-tv": "Travel",
+    "tv-1": "TV1",
+    "vtk": "VTK",
+    "hd-24-kitchen-hd": "24kitchen",
+    "alfa-tv": "Alfa",
+    "axn": "AXN",
+    "axn-black": "AXNBlack",
+    "axn-white": "AXNWhite",
+    "bloomberg-tv": "Bloomberg",
+    // Add more mappings as needed
+};
+
 async function scrapeAndPushToGit() {
     // Clear the sources.m3u file before writing new data
     fs.writeFileSync('sources.m3u', '', 'utf-8');
@@ -14,7 +84,7 @@ async function scrapeAndPushToGit() {
     const page = await browser.newPage();
 
     // Replace this URL with the URL of the page containing the links you want to scrape
-    const url = 'https://www.seir-sanduk.com/?id=bnt-2&pass=55EKLkasldkfa55';
+    const url = 'https://www.seir-sanduk.com/';
     
     await page.goto(url, { waitUntil: 'networkidle2' });
 
@@ -35,8 +105,10 @@ async function scrapeAndPushToGit() {
                 
                 scriptTags.forEach(script => {
                     const scriptContent = script.innerHTML;
-                    if (scriptContent.includes('new Clappr.Player')) {
-                        const match = scriptContent.match(/new Clappr\.Player\s*\(\s*{[^}]*source:\s*["']([^"']+)["']/);
+                    console.log(`Script Content: ${scriptContent}`);  // Log the script content
+                    
+                    if (scriptContent.includes('jwplayer(')) {
+                        const match = scriptContent.match(/file:\s*["']([^"']+)["']/);
                         if (match) {
                             source = match[1];
                         }
@@ -48,7 +120,13 @@ async function scrapeAndPushToGit() {
 
             if (playerSource) {
                 if (playerSource != "https://www.seir-sanduk.com/otustanausta1.mp4") {
-                    const data = `#EXTINF:-1,Player Source\n${playerSource}\n`;
+                    // Extract the channel name from the URL (if it's part of the URL)
+                    const channelId = link.split('id=')[1]?.split('&')[0] || 'Unknown Channel';
+
+                    // Check the mapping for the correct channel name
+                    const channelName = channelNameMapping[channelId] || channelId;
+                    
+                    const data = `#EXTINF:-1,${channelName}\n${playerSource}\n`;
 
                     // Append the data to sources.m3u
                     fs.appendFileSync('sources.m3u', data, 'utf-8');
